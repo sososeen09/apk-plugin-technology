9af933ce52aff7afa2fbb2832de5a287888f3327
yunlong.su <yunlong.su@baidao.com>
2018-04-12 17:53:10 +0800

    插件设置点击事件

--- plugin-demo/src/main/java/com/sososeen09/plugin/demo/MainActivity.java
+++ plugin-demo/src/main/java/com/sososeen09/plugin/demo/MainActivity.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
     private MyReceiver mReceiver;
-
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +22,24 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
+        btnRegister = findViewById(R.id.btn_register);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doRegister();
+            }
+        });
+
+        Log.e("plugin-demo", "onCreate");
+
     }
 
     public void register(View view) {
+
+    }
+
+    private void doRegister() {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("com.sososeen.09.demo.my.receiver");
         mReceiver = new MyReceiver();
